e4657dd476088bc7c14c5f90ce78da9c9971af07
TanjiroBBBBBBBBBBBBB56 <bhempire576@gmail.com>
2025-05-20 23:24:49 +0600

    Update poppunder.js

--- poppunder.js
+++ poppunder.js
@@ -1,5 +1,1 @@
-// poppunder.js
-const popScript = document.createElement('script');
-popScript.type = 'text/javascript';
-popScript.src = '//pl26407515.profitableratecpm.com/79/80/1d/79801d82b52a071baac971d37614f753.js';
-document.body.appendChild(popScript);
+<script type='text/javascript' src='//pl26407515.profitableratecpm.com/79/80/1d/79801d82b52a071baac971d37614f753.js'></script>
